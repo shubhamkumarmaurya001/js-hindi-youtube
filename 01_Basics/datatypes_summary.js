f00595963bc80986e3246f1d6a785e1ed22b9cda
Er. Shubham Maurya <66028723+shubhamkumarmaurya001@users.noreply.github.com>
2025-06-15 15:37:57 +0000

    datatype summary done

---
+++ 01_Basics/datatypes_summary.js
@@ -0,0 +1,37 @@
+// #Primitive data type  (call by value and how it store in memory)
+//7 type :String, Number, boolean, Null, Undefined, Symbol, BigInt
+
+//js is dynamically typed language 
+const score =3;                //number
+const scorevalue=3.0;          //typescript
+
+const isLoggedIn=false;        //boolean
+const outsideTemp=null;        //null
+
+let userEmail;                  //undefined
+let email=undefined;            //undefined
+
+const id=Symbol('123');           //symbol
+const anotherId= Symbol('123');
+console.log(id=== anotherId);
+
+const bigNumber= 22222234758393575356383n;  //BigInt
+
+// #Reference data Type or Non-Primitive (call by reference)
+//Array , Objects ,Functions
+
+const heros =["shaktiman","naagraj","doga"];  //Array
+let myobj={name:"shubham",                     //object
+  age:22
+};
+
+const myfunction= function(){                 //function
+    console.log("hello world");
+}
+
+
+console.log(typeof bigNumber);
+console.log(typeof id);
+console.log(typeof outsideTemp);
+console.log(typeof myfunction);
+
