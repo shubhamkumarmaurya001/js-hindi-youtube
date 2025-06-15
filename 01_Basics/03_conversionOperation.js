72ccda80aa879265e180f2509919fd1cef0abbf6
Er. Shubham Maurya <66028723+shubhamkumarmaurya001@users.noreply.github.com>
2025-06-15 08:10:05 +0000

    conversion of datatype

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -1,8 +1,47 @@
-let score = "hitesh"
+//number
+
+//let score = "33"
+//let score = "33abc"
+//let score = null;
+//let score = true;
+let score = "hitesh";
+
 
 console.log(typeof score);
 console.log(typeof(score));
 
-let valueInNumber = Number(score)
+let valueInNumber = Number (score)
 console.log(typeof valueInNumber);
 console.log(valueInNumber);
+
+//"33"=>33
+//"33abc"=>NaN
+//null=>0
+//true=>1
+//"Hitesh"=>NaN
+
+
+//boolean
+
+//let isLoggedIn=1;          //true
+//let isLoggedIn="";        //false
+//let isLoggedIn="hitesh";    //true
+//let isLoggedIn=null;          //false & object
+//let isLoggedIn=undefined;       //false & Undefined
+let isLoggedIn;                    //false & undefined
+
+
+
+let booleanIsLoggedIn= Boolean(isLoggedIn);
+console.log(typeof (isLoggedIn));
+console.log(typeof (booleanIsLoggedIn));
+console.log(booleanIsLoggedIn);
+
+
+//string
+
+let someNumber = 33;
+let stringNumber=String(someNumber);
+
+console.log(typeof (stringNumber));
+console.log(stringNumber);
