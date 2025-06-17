bfcdfd04ec31a223c67bd5a7d121789f271bafe5
Er. Shubham Maurya <66028723+shubhamkumarmaurya001@users.noreply.github.com>
2025-06-17 15:05:35 +0000

    object part 1 done

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,40 @@
+//singleton
+Object.create
+
+//object literals
+
+const mysym =Symbol("key1");
+
+const Jsuser ={
+    name:"shubham",
+    "fullname":"Shubham maurya",
+    [mysym]:"mykey1",
+    age:18,
+    location:"varanasi",
+    email:"skm@gmail.com",
+    isLoggedIn:false,
+    lastLoginDays:["Monday","Saturday"]
+}
+
+console.log(Jsuser.email);
+console.log(Jsuser.fullname);
+console.log(Jsuser["fullname"]);
+console.log(Jsuser["email"]);
+console.log(Jsuser[mysym]);
+
+Jsuser.email="shubham@chatgpt.com";
+console.log(Jsuser.email);
+
+// Object.freeze(Jsuser);
+
+Jsuser.greeting = function(){
+    console.log("hello");
+}
+Jsuser.greetingTwo = function(){
+    console.log(`hello , ${this.name}`);
+}
+
+console.log(Jsuser);
+console.log(Jsuser.greeting);
+console.log(Jsuser.greeting());
+console.log(Jsuser.greetingTwo());
