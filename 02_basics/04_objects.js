cb4dafcba33bd0373e4c365ffd0cdf708e05ca59
Er. Shubham Maurya <66028723+shubhamkumarmaurya001@users.noreply.github.com>
2025-06-17 16:36:01 +0000

    object destructuring

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -43,13 +43,25 @@ const users=[
     }
 ]
 
-console.log(users[1].email);
+// console.log(users[1].email);
 
-console.log(tinderUser);
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(tinderUser);
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
 
 
+//object destructuring
+
+const course={
+    coursename:"js in hindi",
+    price:999,
+    courseinstructor:"hitesh"
+}
+
+const {coursename:cn,courseinstructor:ci}=course;
+// console.log(coursename);
+console.log(cn);
+console.log(ci);
