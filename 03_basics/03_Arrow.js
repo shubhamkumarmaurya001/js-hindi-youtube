0d23311c5b64196a33ec90a9b528ef78e3cdff99
Er. Shubham Maurya <66028723+shubhamkumarmaurya001@users.noreply.github.com>
2025-06-18 14:38:46 +0000

    this and arrow done

---
+++ 03_basics/03_Arrow.js
@@ -0,0 +1,49 @@
+const user={
+    username:"Hitesh",
+    price:999,
+
+    welcomeMessage:function(){
+        console.log(`${this.username}, welcome to website`);
+        console.log(this);
+    }
+}
+
+// user.welcomeMessage();
+// user.username="sam";
+// user.welcomeMessage();
+
+// console.log(this);
+
+
+// this not work inside function as contest
+
+// function chai(){
+//     let username="Hitesh"
+//     console.log(this.username);   //here, username is contest  
+//     console.log(this);
+// }
+
+// chai();
+
+
+//Arrow fumction
+// const chai= function(){
+//     const username="hitesh";
+//     console.log(this.username);
+//     console.log(this);
+// }
+const chai= ()=>{
+    const username="hitesh";
+    console.log(this.username);
+    console.log(this);
+}
+// chai();
+
+
+// const addTwo = (num1,num2)=>{
+//        return num1+num2;
+// }
+// const addTwo = (num1,num2)=> num1+num2;  //implicit return arrow function
+// const addTwo = (num1,num2)=> ( num1+num2 );  //implicit return arrow function inside parenthesis no need to write return keyword
+const addTwo = (num1,num2)=> ( {username:"Hitesh"} ); 
+console.log(addTwo(3,4));
