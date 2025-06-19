98964cdd46fff0fe79ecc7b47996aa62a4f5e693
Er. Shubham Maurya <66028723+shubhamkumarmaurya001@users.noreply.github.com>
2025-06-19 11:13:41 +0000

    while and do while done

---
+++ 05_iteration/two.js
@@ -0,0 +1,25 @@
+//while
+// while (condition) {
+    
+// }
+
+// let index=0;
+// while (index<=10) {
+//     console.log(`value of index ${index}`);
+//     index++;
+// }
+
+// let array=["flash", "batman", "superman"];
+// let index=0;
+// while(index<array.length){
+//     console.log(`value of array is ${array[index]}`);    
+//     index++;
+// }
+
+
+//do while
+let score =1;
+do {
+console.log(`score is ${score}`);
+score++;
+} while (score<=10);
