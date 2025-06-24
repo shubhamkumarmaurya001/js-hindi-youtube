5dba321dce039830f06c56cff5a6f497024ebbb9
Er. Shubham Maurya <66028723+shubhamkumarmaurya001@users.noreply.github.com>
2025-06-24 16:36:32 +0000

    project 2 bmi calclator done

---
+++ 07_projects/2-BmiCalculator/chaiaurcode.js
@@ -0,0 +1,31 @@
+const form= document.querySelector('form');
+//this usecase will give you empty value
+// const height= parseInt(document.querySelector('#height').value)
+
+form.addEventListener('submit',function (e){
+    e.preventDefault();
+    const height= parseInt(document.querySelector('#height').value);
+    const weight= parseInt(document.querySelector('#weight').value);
+    const results= document.querySelector('#results');
+    const hint= document.querySelector('#hint');
+
+    if(height==='' || height<0 || isNaN(height)){
+        results.innerHTML=`please give a valid height ${height}`;
+    }else if(weight==='' || weight<0 || isNaN(weight)){
+        results.innerHTML=`please give a valid weight ${weight}`;
+    }else{
+        const bmi =(weight/((height*height)/10000)).toFixed(2);
+        //show the result
+        results.innerHTML=`<span>${bmi}</span>`
+        if(bmi<18.6){
+            hint.innerHTML=`${bmi} is under weight`
+        }else if(bmi>18.6 && bmi<24.9){
+            hint.innerHTML=`${bmi} is normal`
+        }else{
+            hint.innerHTML=`${bmi} is overweight`
+        }
+    }
+    
+
+
+})
